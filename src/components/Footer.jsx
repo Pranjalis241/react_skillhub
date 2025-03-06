5d39f328870476808b9274f8fe4d996a84a24a50
Pranjali <pranjalis241@gmail.com>
2025-03-06 11:38:31 +0530

    footer responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,15 @@ import "./Footer.css";
 const Footer = () => {
     return (
         <footer className="bg-dark text-white text-center py-3">
-            <p>© 2025 SkillHub. All Rights Reserved.</p>
-            <div class="footer-contact">
-      <h3>Contact Us:</h3>
-      <p><i class="fa-solid fa-envelope"></i> Email: skillhub@gmail.com</p>
-      <p><i class="fa-solid fa-phone"></i> Phone: +91 9972143214</p>
-      <p><i class="fa-solid fa-location-dot"></i> Address: Bangalore, India</p>
-    </div>
+            <div className="container">
+                <p>© 2025 SkillHub. All Rights Reserved.</p>
+                <div className="footer-contact d-flex flex-column flex-lg-row align-items-center justify-content-center">
+                    <h5 className="mb-2 mb-lg-0">Contact Us:</h5>
+                    <p><i className="fa-solid fa-envelope"></i> Email: skillhub@gmail.com</p>
+                    <p><i className="fa-solid fa-phone"></i> Phone: +91 9972143214</p>
+                    <p><i className="fa-solid fa-location-dot"></i> Address: Bangalore, India</p>
+                </div>
+            </div>
         </footer>
     );
 };
